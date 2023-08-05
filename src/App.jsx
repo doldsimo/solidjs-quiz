697aef68699d500d8d0bf1486ba20a7b0c95d72b
doldsimo <simon.dold@gmx.de>
2023-08-05 16:34:30 +0200

    adding base to router

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const config = {
 function App() {
   return (
     <HopeProvider config={config}>
-      <Router>
+      <Router base='solid-quiz'>
         <DarkModeButton />
         {/* <Heading>My Site with Lots of Pages</Heading> */}
         <br />
